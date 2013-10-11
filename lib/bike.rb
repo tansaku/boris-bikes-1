e8f9dd77ee286f6ad51a1105444e3ec71f254e68
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-10-11 15:19:28 +0100

    Implemented broken? method

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,22 @@
 class Bike
   
-  def broken?
+  # the initialize method is always called when you create a new
+  # class by typing Bike.new
+  def initialize
+    # all instance variables begin with "@"
+    # this must be an instance variable because we'll need it
+    # in other methods
+    @broken = false
   end
 
+  def broken?
+    # instance variables are accessible in all methods
+    @broken
+  end
+
+  def break
+    # and any instance method can update them
+    @broken = true
+  end
+  
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -11,4 +11,10 @@ describe Bike do
     expect(bike).not_to be_broken
   end
 
+  it "should be able to break" do
+    bike = Bike.new
+    bike.break
+    expect(bike).to be_broken
+  end
+
 end

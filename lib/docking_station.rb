b3801f76063a5162880675f443601294363ff73a
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-10-15 16:23:32 +0100

    Extracted DockingStation methods into a module

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,31 +1,15 @@
+# load BikeContainer
+require_relative 'bike_container'
+
 class DockingStation
 
-  DEFAULT_CAPACITY = 10
+  # this gives us all the methods that used to be in this class
+  include BikeContainer
 
-  def initialize(options = {})    
-    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-    @bikes = []
-  end
-
-  def bike_count
-    @bikes.count
-  end
-
-  def dock(bike)    
-    raise "Station is full" if full?
-    @bikes << bike
-  end
-
-  def release(bike)
-    @bikes.delete(bike)
-  end
-
-  def full?
-    bike_count == @capacity
-  end
-
-  def available_bikes
-    @bikes.reject {|bike| bike.broken? }
+  def initialize(options = {})
+    # self.capacity is calling the capacity=() method (note the equals sign) defined in BikeContainer
+    # capacity (the second argument to fetch()) is calling the capacity() method in BikeContainer
+    self.capacity = options.fetch(:capacity, capacity)
   end
 
 end
